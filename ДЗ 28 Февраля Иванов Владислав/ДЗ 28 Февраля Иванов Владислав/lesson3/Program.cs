3c36afabf7088899e9dc2b63822cafc2b5aacb1c
VlaDKuT <vio-94@mail.ru>
2019-03-03 22:31:01 +0300

    Задача 3. Данный проект содержит закомментированное решение по 1 и 2 задаче

---
+++ ДЗ 28 Февраля Иванов Владислав/ДЗ 28 Февраля Иванов Владислав/lesson3/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson3
+{
+    #region Текст задания 1 и 2
+    //Дан целочисленный  массив из 20 элементов.Элементы массива  могут принимать  целые значения  от –10 000 до 10 000 включительно.
+    //Заполнить случайными числами.Написать программу, позволяющую найти и вывести количество пар элементов массива, в которых только одно число делится на 3. 
+    //В данной задаче под парой подразумевается два подряд идущих элемента массива.Например, для массива из пяти элементов: 6; 2; 9; –3; 6 ответ — 2. 
+
+    // 2) Реализуйте задачу 1 в виде статического класса StaticClass;
+    // а) Класс должен содержать статический метод, который принимает на вход массив и решает задачу 1;
+    #endregion
+
+    #region Задание 3
+    //3)
+    //а) Дописать класс для работы с одномерным массивом.
+    //Реализовать конструктор, создающий массив определенного размера и заполняющий массив числами от начального значения с заданным шагом.
+    //Создать свойство Sum, которое возвращает сумму элементов массива, 
+    //метод Inverse,возвращающий новый массив с измененными знаками у всех элементов массива(старый массив, остается без изменений),  
+    //метод Multi, умножающий каждый элемент массива на определённое число, 
+    //свойство MaxCount, возвращающее количество максимальных элементов.
+    #endregion
+
+    class Program
+    {
+        /// <summary>
+        /// Метод переводящий введенные значения пользователя в число
+        /// </summary>
+        /// <returns></returns>
+        public static int TextToInt ()
+        {
+            string textString = Console.ReadLine();//Вводим размер массива
+            int number = Convert.ToInt32(textString);//переводим введенну. строк в число
+            return number;
+        }
+
+        static void Main(string[] args)
+        {
+        
+            #region Задание 1 - 2
+            //int[] mass = new int[20];
+            //mass = StaticClass.RandMass(mass);
+            //StaticClass.TextWrite(mass);
+
+            //Console.WriteLine("\nСортируем элементы массива");
+            //Array.Sort(mass);
+            //StaticClass.TextWrite(mass);
+
+            //int pair = StaticClass.Task(mass);
+            //Console.WriteLine("\nКоличество пар, которые деляться на 3  - " + pair);
+            #endregion
+
+            Console.Write("Привет! Данная программа демнстрирует работы класса для работы с массивом.\nДавай создадим новый массив NewMass и наполняем его значениями от - 10 000 до 10 000.\nСколько будет элементов в массиве: ");
+            int NewLengthMass = TextToInt();
+
+            int[] NewMass = StaticClass.CreatArray(NewLengthMass);
+            Console.Write($"\nНовый массив содержит следующие значения - NewMass: ");
+            StaticClass.TextWrite(NewMass);
+            Console.ReadKey();
+
+            int sum = StaticClass.SumMass;
+            Console.Write($"\nС помощью свойства Sum определяем сумма элементов массива - {sum}");
+            Console.ReadKey();
+
+            int[] NewMass1 = StaticClass.Inverse(NewMass);
+            Console.Write($"\nС помощью метода Inverse новый массив NewMass1 заполняем значениями массива NewMass, но меняем знаки значений массива на противоположный.\nNewMass1 - ");
+            StaticClass.TextWrite(NewMass1);
+            Console.ReadKey();
+
+            Console.Write($"\nС помощью метода Multi умножаем каждый элемент массива NewMass1.\nНа какое число умножить элементы:");
+            int mult = TextToInt();
+            NewMass1 = StaticClass.Multi(NewMass1, mult);
+            StaticClass.TextWrite(NewMass1);
+            Console.ReadKey();
+
+            Console.Write($"\nС помощью свойства MaxCount найдем количество элементов массива NewMass1, значения которых больше 0:");
+            StaticClass.Mass = NewMass1;
+            int coun = StaticClass.MaxCount;
+            Console.Write($"\nВ массиве {coun} положительных(ый) элемент(а)");
+
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ ДЗ 28 Февраля Иванов Владислав/ДЗ 28 Февраля Иванов Владислав/lesson3/StaticClass.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson3
+{
+    class StaticClass
+    {
+        /// <summary>
+        /// Глобальная переменная для работы в классе StaticClass
+        /// </summary>
+        public static  int[] Mass;
+
+        /// <summary>
+        /// Метод создающий новый массив
+        /// </summary>
+        /// <param name="LengthMass"></param>
+        /// <returns></returns>
+        public static int[] CreatArray(int LengthMass)
+        {
+            Mass = new int[LengthMass];
+            Mass = RandMass(Mass);
+            return Mass;
+        }
+
+        /// <summary>
+        /// Методж заполняющий массив рандомными числами
+        /// </summary>
+        /// <param name="mass"></param>
+        /// <returns></returns>
+        public static int[] RandMass(int[] mass)
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = r.Next(-10000, 10000);
+            }
+            return mass;
+        }
+
+        /// <summary>
+        /// Метод, который выводит значение элемнтов массива на экран
+        /// </summary>
+        /// <param name="arry"></param>
+        public static void TextWrite(int[] arry)
+        {
+            for (int i = 0; i < arry.Length; i++)
+            {
+                Console.Write($"[{arry[i]}]");
+            }
+
+        }
+
+        /// <summary>
+        /// Метод ищущий количество пар в массиве, которые деляться на 3
+        /// </summary>
+        /// <param name="arry"></param>
+        /// <returns></returns>
+        public static int Task(int[] arry)
+        {
+            int pair = 0;
+            for (var i = 0; i < arry.Length - 1; i++)
+            {
+                if (arry[i] % 3 == 0 && arry[i + 1] % 3 == 0)
+                {
+                    pair++;
+                }
+            }
+            return pair;
+        }
+
+        /// <summary>
+        /// Свойство показывает сумму значений элементов массива
+        /// </summary>
+        public static int SumMass
+        {
+            get
+            {
+                int sum = 0;
+                for (var i = 0; i < Mass.Length; i++)
+                {
+                    sum = sum + Mass[i];
+                }
+                return sum;
+            }
+
+        }
+
+        /// <summary>
+        /// Метод меняющий значки значений элементов массива на противоположный
+        /// </summary>
+        /// <param name="Mass"></param>
+        /// <returns></returns>
+        internal static int[] Inverse(int[] Mass)
+        {
+            for (int i = 0; i < Mass.Length; i++)
+            {
+                Mass[i] = Mass[i] * (-1);
+            }
+            return Mass;
+        }
+
+        /// <summary>
+        /// Метод множающий каждый элемент массива на указанное число
+        /// </summary>
+        /// <param name="Mass"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        internal static int[] Multi(int[] Mass, int n)
+        {
+            for (int i = 0; i < Mass.Length; i++)
+            {
+                Mass[i] = Mass[i] * (n);
+            }
+            return Mass;
+        }
+
+        /// <summary>
+        /// Свойство которое возвращает максимальное количество элемнтов в массиве
+        /// </summary>
+        /// <param name="Mass"></param>
+        /// <returns></returns>
+        internal static int MaxCount
+        {
+            get
+            {
+                int Max = 0;
+                for (int i = 0; i < Mass.Length; i++)
+                {
+                   if (Mass[i] > 0)
+                   {
+                        Max++;
+                    }                        
+                }
+                
+                return Max;
+
+            }
+            
+        }
+    }
+}
